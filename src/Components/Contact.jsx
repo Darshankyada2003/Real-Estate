0dc9274a219c043d98e577bbab628e77daaca8b7
darshan kyada <darshankyada02@gmail.com>
2025-01-29 16:42:22 +0530

    framer motion animation in all section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { toast } from 'react-toastify';
+import { motion } from 'framer-motion';
 
 const Contact = () => {
     const [result, setResult] = React.useState("");
@@ -18,7 +19,7 @@ const Contact = () => {
 
         const data = await response.json();
 
-        if (data.success) { 
+        if (data.success) {
             setResult("");
             toast.success("Form Submitted Successfully")
             event.target.reset();
@@ -29,7 +30,12 @@ const Contact = () => {
         }
     };
     return (
-        <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+        <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
             <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span
                 className='underline underline-offset-4 decoration-1 under font-light'>
                 With Us</span></h1>
@@ -55,11 +61,11 @@ const Contact = () => {
                         className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none'
                         name='Meassage' placeholder='Message' required></textarea>
                 </div>
-                <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>
+                <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded cursor-pointer'>
                     {result ? result : "Send Message"}
                 </button>
             </form>
-        </div>
+        </motion.div>
     )
 }
 
