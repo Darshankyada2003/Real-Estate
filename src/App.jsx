4342caf3123bc10170ef57660e59747db141ed1e
darshan kyada <darshankyada02@gmail.com>
2025-01-29 14:52:27 +0530

    Make a Contact Us Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Header from './Components/Header'
 import About from './Components/About'
 import Projects from './Components/Projects'
 import Testimonails from './Components/Testimonails'
+import Contact from './Components/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonails />
+      <Contact />
     </div>
   )
 }
